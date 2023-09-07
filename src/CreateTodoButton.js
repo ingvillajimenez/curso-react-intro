5aafe78be55b01f7d7802c9b3c363c8e59f7a4b7
Juan Villa <ing.villa.jimenez@gmail.com>
2023-09-07 10:19:28 -0600

    Eventos en React: onClick, onChange

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./CreateTodoButton.css";
 
 function CreateTodoButton() {
-  return <button className="CreateTodoButton">+</button>;
+  return (
+    <button
+      className="CreateTodoButton"
+      onClick={(event) => {
+        console.log("le diste click");
+        console.log(event);
+        console.log(event.target);
+      }}
+    >
+      +
+    </button>
+  );
 }
 
 export { CreateTodoButton };
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
-  return <input className="TodoSearch" placeholder="Cortar cebolla" />;
+  return (
+    <input
+      className="TodoSearch"
+      placeholder="Cortar cebolla"
+      onChange={(event) => {
+        console.log("Escribiste en el TodoSearch");
+        console.log(event);
+        console.log(event.target);
+        console.log(event.target.value);
+      }}
+    />
+  );
 }
 
 export { TodoSearch };
