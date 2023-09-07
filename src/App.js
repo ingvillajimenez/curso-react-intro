dfa96513910453d3cd1cc53644cbfb837f661fa8
Juan Villa <ing.villa.jimenez@gmail.com>
2023-09-07 07:06:54 -0600

    Como se comunican los componentes? Props y atributos

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import { TodoCounter } from "./TodoCounter";
 import { TodoSearch } from "./TodoSearch";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
 import "./App.css";
+
+const defaultTodos = [
+  { text: "Cortar cebolla", completed: true },
+  { text: "Tomar el Curso de Intro a React.js", completed: false },
+  { text: "Llorar con la llorona", completed: false },
+  { text: "LALALALA", completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <TodoCounter />
+    <React.Fragment>
+      <TodoCounter completed={16} total={25} />
       <TodoSearch />
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
       </TodoList>
-      {/* <CreateTodoButton /> */}
-    </div>
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
