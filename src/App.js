1f48f1a3e183fc13454d614ff3e3c05553dd4290
Juan Villa <ing.villa.jimenez@gmail.com>
2023-09-07 09:42:42 -0600

    Estilos CSS es React

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { TodoSearch } from "./TodoSearch";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
-import "./App.css";
 
 const defaultTodos = [
   { text: "Cortar cebolla", completed: true },
@@ -15,7 +14,7 @@ const defaultTodos = [
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <TodoCounter completed={16} total={25} />
       <TodoSearch />
       <TodoList>
@@ -28,7 +27,7 @@ function App() {
         ))}
       </TodoList>
       <CreateTodoButton />
-    </React.Fragment>
+    </>
   );
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./TodoList.css";
 
 function TodoList({ children }) {
-  return <ul>{children}</ul>;
+  return <ul className="TodoList">{children}</ul>;
 }
 
 export { TodoList };
