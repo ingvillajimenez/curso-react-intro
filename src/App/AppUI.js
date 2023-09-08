39970e5c526ab02fc26dc1898ff36655629d5921
Juan Villa <ing.villa.jimenez@gmail.com>
2023-09-08 14:20:18 -0600

    Tips para naming y abstraccion de componentes React

---
+++ src/App/AppUI.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { TodoCounter } from "../TodoCounter";
+import { TodoSearch } from "../TodoSearch";
+import { TodoList } from "../TodoList";
+import { TodoItem } from "../TodoItem";
+import { CreateTodoButton } from "../CreateTodoButton";
+
+function AppUI({
+  completedTodos,
+  totalTodos,
+  searchValue,
+  setsearchValue,
+  searchedTodos,
+  completeTodo,
+  deleteTodo,
+}) {
+  return (
+    <>
+      <TodoCounter completed={completedTodos} total={totalTodos} />
+      <TodoSearch searchValue={searchValue} setsearchValue={setsearchValue} />
+      <TodoList>
+        {searchedTodos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+    </>
+  );
+}
+
+export { AppUI };
