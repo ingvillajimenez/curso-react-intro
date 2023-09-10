002549e36a5001e095e24400c9a5fa143be341f9
Juan Villa <ing.villa.jimenez@gmail.com>
2023-09-09 19:02:33 -0600

    ¿Que es React Context?

--- src/TodoContext/useLocalStorage.js
+++ src/TodoContext/useLocalStorage.js
@@ -38,3 +38,15 @@ function useLocalStorage(itemName, initialValue) {
 }
 
 export { useLocalStorage };
+
+// localStorage.removeItem("TODOS_V1");
+
+// const defaultTodos = [
+//   { text: "Cortar cebolla", completed: true },
+//   { text: "Tomar el Curso de Intro a React.js", completed: false },
+//   { text: "Llorar con la llorona", completed: false },
+//   { text: "LALALALA", completed: false },
+//   { text: "Usar estados derivados", completed: true },
+// ];
+
+// localStorage.setItem("TODOS_V1", JSON.stringify(defaultTodos));
