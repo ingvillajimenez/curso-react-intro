94fa3ab7cb92906cde4ef0611c3fa60606f3da0c
Juan Villa <ing.villa.jimenez@gmail.com>
2023-09-09 22:43:49 -0600

    ¿Qué son los React Portals?

--- src/TodoContext/index.js
+++ src/TodoContext/index.js
@@ -11,6 +11,7 @@ function TodoProvider({ children }) {
     error,
   } = useLocalStorage("TODOS_V1", []);
   const [searchValue, setsearchValue] = React.useState("");
+  const [openModal, setOpenModal] = React.useState(true);
 
   const completedTodos = todos.filter((todo) => todo.completed).length;
   const totalTodos = todos.length;
@@ -48,6 +49,8 @@ function TodoProvider({ children }) {
         searchedTodos,
         completeTodo,
         deleteTodo,
+        openModal,
+        setOpenModal,
       }}
     >
       {children}
